4c96ae9eb7fca9943cb7de34b116bf13bbb97552
leonardo suarez <leonardofab19@hotmail.com>
2021-01-04 17:27:14 -0300

    sin cambios

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//variables 
+const carrito = document.querySelector('#carrito');
+
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+
+const listaCursos = document.querySelector('#lista-cursos');
