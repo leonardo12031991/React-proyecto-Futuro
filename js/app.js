a41924da286ac07dd72fefede0c28362fda6d592
leonardo suarez <leonardofab19@hotmail.com>
2021-01-08 20:36:55 -0300

    Cambios en javaScript funciones en carrito

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
+let articuloCarrito = [];
 
 cargarEventosListeners();
 
@@ -25,12 +26,39 @@ function agregarArticulo(e) {
 }
 //lee el contenido del HTML al que le damos click y extre información
 function leerDatos(articulo) {
-    //console.log(articulo);
+    console.log(articulo);
 
 //crear un objeto con el contenido del articulo
 const infoArticulo = {
-    imagen: articulo.querySelector('img').src
+    imagen: articulo.querySelector('img').src,
+    titulo:articulo.querySelector('h4').textContent,
+    precio:articulo.querySelector('.precio span').textContent,
+    id: articulo.querySelector('a').getAttribute('data-id'),
+    cantidad:1
     
   }
-  console.log(infoArticulo);
+  //Agrega elementos al arreglo del carrito
+  articuloCarrito=[...articuloCarrito, infoArticulo];
+  //console.log(articuloCarrito);
+  carritoHTML();
+}
+
+//Muestra los articulos del carrito en el HTML
+function carritoHTML() {
+    //limpiar el HTML
+
+
+    
+    //Recorre el carrito y genera el html
+ articuloCarrito.forEach(articulo =>{
+     const row= document.createElement('tr');
+     row.innerHTML=`
+     <td>
+        ${articulo.titulo}
+
+     </td>         
+     `;
+     //agrega el HTML de el carrito al tbody
+     contenedorCarrito.appendChild(row)
+ })
 }
